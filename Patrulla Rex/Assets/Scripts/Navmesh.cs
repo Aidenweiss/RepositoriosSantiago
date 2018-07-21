5db7bb9ddda93b0e24853988df24406b52b46062
Aidenweiss <santiagoagj@gmail.com>
2018-07-21 12:52:13 -0500

    patrulla 1
    jodidos con el navmesh

---
+++ Patrulla Rex/Assets/Scripts/Navmesh.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace mapa
+{
+    public class Navmesh : MonoBehaviour
+    {
+        public NavMeshAgent agent;
+        public Transform target;
+
+        // Use this for initialization
+        void Start ()
+        {
+		
+	    }
+	
+	    // Update is called once per frame
+	    void Update ()
+        {
+            Meta();
+	    }
+        public void Meta()
+        {
+            agent.SetDestination(target.position);
+        }
+        public void Kawake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+    }
+    
+}
---
+++ Patrulla Rex/Assets/Scripts/Patrullaje.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using mapa;
+
+public class Patrullaje : Navmesh
+{
+    public Transform position;
+    public GameObject[] waypoints;
+    public float range;
+    public float eSpeed;
+    public float iSpeed;
+    public float tiempo;
+    public int i;
+
+    void Awake()
+    {
+        Kawake();
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        waypoints = GameObject.FindGameObjectsWithTag("WayPoint");
+       // position = GameObject.Find("Hero").transform;
+        iSpeed = eSpeed;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Patrullar();
+        agent.SetDestination(waypoints[i].transform.position);
+
+    }
+    void Patrullar()
+    {
+        if (i < waypoints.Length)
+        {
+            float direction = (waypoints[i].transform.position - transform.position).magnitude;
+            if (direction > range)
+            {
+               
+                transform.position += Vector3.Normalize(waypoints[i].transform.position - transform.position) * eSpeed * Time.deltaTime;
+
+            }
+            if (direction < range)
+            {
+               i = Random.Range(0,waypoints.Length);
+            }
+           
+        }
+        else
+        {
+            if (tiempo > 0)
+            {
+                eSpeed = 0;
+                tiempo -= Time.deltaTime;
+            }
+            else
+            {
+                i = 0;
+                tiempo = 1;
+                eSpeed = iSpeed;
+            }
+        }
+    }
+}
